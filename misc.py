eeb97b6826e41726177705a2242b22600a717862
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-07-21 15:59:42 -0700

    Pollen: Add ZipCode and Data Validation, Simplify 'records' to 'db'
    - Add pretty representation
    - Sort methods alphabetically
    - Require data refresh after source or zipcode change
    - Add custom exception and tests

--- misc.py
+++ misc.py
@@ -25,14 +25,14 @@ def human_hex(dec):
     return hex(dec)[2:]
 
 def map_args(function, iterable, *args, **kwargs):
-    """Maps iterable to a function with arguments/keywords. 
+    """Maps iterable to a function with arguments/keywords.
        Dynamic types can be used"""
     return type(iterable)(function(x, *args, **kwargs) for x in iterable)
 
 def map_args_test(x, y=2, z=3):
     """A test function for the map_args function. Returns the sum of x, y, z"""
     return x + y + z
-    
+
 class Pollen(object):
 
     """Class Pollen can be used to retrieve and store information about
@@ -51,30 +51,26 @@ class Pollen(object):
 
     def __init__(self, source, zipcode=98105, print_sources=True):
         """Constructs a new Pollen object using the given source and zipcode"""
+        if not(zipcode in Pollen.SOURCE_URLS.keys()):
+            raise ZipCodeNotFoundException(zipcode)
         self.zipcode = zipcode
         self.print_sources = print_sources
         self.source = source
         self.setsource(source)
         self.setzipcode(zipcode)
+        self.__has_built = False
         self.build()
-
-    def setzipcode(self, zipcode):
-        """Sets the zipcode for this Pollen object. Requires `build` to be called to update data"""
-        if self.source != 'weather text' and not(zipcode in Pollen.SOURCE_URLS.keys()):
-            raise ValueError('only zipcode(s) {} are supported for values or Wunderground'.format(', '.join(SOURCE_URLS.keys())))
-        assert zipcode >= 0 and zipcode <= 99501 # valid US zipcode
-        self.zipcode = zipcode
-        self.setsource(self.source)
         
-    def setsource(self, source):
-        """Sets the source for this Pollen object. Requires `build` to be called to update data"""
-        assert source in Pollen.SOURCE_SPAN.keys()
-        self.uri = Pollen.SOURCE_URLS[self.zipcode][source]
-        if source == 'weather text':
-            self.uri += str(self.zipcode)
-        self.source = source
+    def __repr__(self):
+        """Returns the string representation of this Pollen instance"""
+        return f'Pollen(source={{{self.source}}}, zipcode={self.zipcode}, print_sources={self.print_sources})'
 
-    def _get_markup(self, uri):
+    def __check_built(self):
+        """Throws a RuntimeError if this Pollen instance has not been built"""
+        if not(self.__has_built):
+            raise RuntimeError('Pollen must be build after zipcode or source has been changed')
+
+    def __get_markup(self, uri):
         """Retrieves the markup with up to 4 max tries"""
         if self.source == 'weather text':
             params = _WEB_HDR
@@ -94,90 +90,147 @@ class Pollen(object):
             print()
         return req.content
 
-    def build(self, weather_query=True):
+    def build(self, add_weather_query=True):
         """Builds and populates the pollen record database"""
         uri = self.uri
-        if self.source == 'weather text' and weather_query:
+        if self.source == 'weather text' and add_weather_query:
             uri += self.WEATHER_QUERY_PARAMS
-        markup = self._get_markup(uri)
+        markup = self.__get_markup(uri)
 
         page = _BeautifulSoup(markup, 'html.parser')
 
         if self.source == 'weather text':
             found = page.select('button > div')
-            records = dict()
+            db = dict()
             if len(found) > 0:
                 for elm in found:
                     divs = elm.select('div')
-                    records[divs[0].getText()] = divs[-1].getText()
+                    db[divs[0].get_text()] = divs[-1].get_text()
         elif self.source == 'weather values':
             js = _json.loads(markup)
             base = js['pollenForecast12hour']
             stored = list(base[layer + 'PollenIndex'] for layer in Pollen.TYPES)
             lzt = list(zip(*stored))
-            records = {i / 2: lzt[i] for i in range(len(lzt))}
+            db = {i / 2: lzt[i] for i in range(len(lzt))}
         else:
             j = page.select('.count') # or class .status
-            records = {i: j[i].getText() for i in range(Pollen.SOURCE_SPAN[self.source])}
-        if len(records) == 0:
+            db = {i: j[i].get_text() for i in range(Pollen.SOURCE_SPAN[self.source])}
+        if len(db) == 0:
             if self.source == 'weather text':
-                self.build(weather_query=not(weather_query))
+                self.build(add_weather_query=not(add_weather_query)) # retry using the alternate query
             else:
-                records = {i: 'null' for i in range(Pollen.SOURCE_SPAN[self.source])['null']}
+                db = {i: 'null' for i in range(Pollen.SOURCE_SPAN[self.source])['null']}
         self.src = page
-        self.records = records
-
-    def printall(self):
-        """Prints all of the records information in a table format"""
-        print('Pollen data for', self.zipcode)
-        for i, j in self.records.items():
-            print('{:>{}}: {}'.format(i, len('Tonight'), j))
+        self.db = db
+        self.__has_built = True
 
     def get_day(self, day):
-        """Returns the value in the records for the given day"""
+        """Returns the value in the db for the given day"""
+        self.__check_built()
         data = None
         if self.source == 'weather text':
             date = str((_dt.date.today() + _dt.timedelta(days=day)).day)
-            for dong in self.records:
+            for dong in self.db:
                 if dong.endswith(' ' + date):
-                    data = self.records[dong]
+                    data = self.db[dong]
                     break # not the best style but saves runtime
         else:
             # updates afternoon forecasts for today only (floor of cos of day)
             day += 0.5 * _math.floor(_math.cos(day)) * _math.floor(_dt.datetime.now().hour / 12)
-            if type(self.records[day]) == str:
-                data = self.records[int(day)].title()
+            if type(self.db[day]) == str:
+                data = self.db[int(day)].title()
             else:
-                data = self.records[day]
+                data = self.db[day]
         if data is not None and self.print_sources:
             print('[{}] day={}'.format(self.source, day))
         return data
 
     def get_today(self):
         """Returns the type of pollen for today"""
+        self.__check_built()
         if self.source == 'weather text':
-            if 'Tonight' in self.records:
-                return self.records['Tonight']
-            elif 'Today' in self.records:
-                return self.records['Today']
+            if 'Tonight' in self.db:
+                return self.db['Tonight']
+            elif 'Today' in self.db:
+                return self.db['Today']
         return self.get_day(0)
 
     def get_tomorrow(self):
         """Returns the type of pollen forecasted for tomorrow"""
-        return self.get_day(1)
+        return self.get_day(1) # checks for valid db in get_day
+        
+    def printall(self):
+        """Prints all of the db information in a table format"""
+        self.__check_built()
+        print('Pollen data for', self.zipcode)
+        for i, j in self.db.items():
+            print('{:>{}}: {}'.format(i, len('Tonight'), j))
+
+    def setsource(self, source):
+        """Sets the source for this Pollen object. Requires `build` to be called to update data"""
+        if not(source in Pollen.SOURCE_SPAN.keys()):
+            raise ValueError(f'source must be one from [{", ".join(Pollen.SOURCE_SPAN.keys())}]')
+        self.uri = Pollen.SOURCE_URLS[self.zipcode][source]
+        if source == 'weather text':
+            self.uri += str(self.zipcode)
+        self.source = source
+        self.__has_built = False
+
+    def setzipcode(self, zipcode):
+        """Sets the zipcode for this Pollen object. Requires `build` to be called to update data"""
+        if (self.source != 'weather text' and not(zipcode in Pollen.SOURCE_URLS.keys())) or \
+            not(zipcode in Pollen.SOURCE_URLS.keys()) or zipcode < 0 or zipcode > 99501:
+            raise ZipCodeNotFoundException(zipcode)
+        self.zipcode = zipcode
+        self.setsource(self.source) # ensures data is updated if the method is 'weather text'
+
+class ZipCodeNotFoundException(Exception):
+    def __init__(self, zipcode, *args, **kwargs):
+        super(ZipCodeNotFoundException, self).__init__(repr(self), *args, **kwargs)
+        self.zipcode = zipcode
+
+    def __repr__(self):
+        zipcodes = ", ".join(map(str, Pollen.SOURCE_URLS.keys()))
+        plural = zipcodes.count(',') > 0
+        string = 'The only zipcode'
+        if plural:
+            string += 's'
+        string += ' currently supported for Pollen '
+        if plural:
+            string += 'are'
+        else:
+            string += 'is'
+        string += ' ' + zipcodes
+        return string
 
 if __name__ == '__main__':
-
+    
+    import sys
+    import traceback
+    
     print('human hex for 2700', human_hex(2700))
     print('map args on test function', map_args(map_args_test, (1, 4, 16, 25), z = 4))
 
-    p = Pollen('weather text', zipcode=97132)
+    p = Pollen('weather text')
     p.printall()
     p.setsource('weather values')
     p.setzipcode(98105)
     p.build()
     p.printall()
     
+    print('The next two tests will fail.')
+    try:
+        p.setsource('wrong source')
+    except Exception:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_tb)
+    print()
+    try:
+        p.setzipcode(97132)
+    except Exception:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_tb)
+
     # other unit tests
     # for i in Pollen.SOURCE_SPAN.keys():
         # p.setsource(i)
