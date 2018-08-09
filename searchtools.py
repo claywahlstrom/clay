faff0290859136e5a9982ed5a9f076cb43504a0a
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-08-08 23:08:45 -0700

    searchtools: Update exclusion conditions, add content file search
    - Wrap result matcher in try-except
    - Change extension input parameter type
    - Add search options to result string

--- searchtools.py
+++ searchtools.py
@@ -17,15 +17,18 @@ from clay.shell import getDocsFolder as _getDocsFolder, isIdle as _isIdle
 
 
 EXCLUDED = ['.android', '.AndroidStudio1.5', 'eclipse', '.gradle', '.idlerc',
-           '.jmc', '.matplotlib', '.oracle_jre_usage', '.pdfsam', '.phet',
-           '3D Objects', 'AppData', 'Application Data', 'eclipse', 'Android']
+            '.jmc', '.matplotlib', '.oracle_jre_usage', '.pdfsam', '.phet',
+            '3D Objects', 'AppData', 'Application Data', 'eclipse', 'Android',
+            'NuGet']
 
 STR_LIM = 75 # path printing termination number
 
 class Search(object):
     """A class for searching a file system"""
-    def __init__(self, method='name', folder='.', string=str(), ext=False):
-        """Searches files by method, folder, string, ext"""
+    def __init__(self, method='name', folder='.', string=str(), ext=None):
+        """Searches files by method, folder, regex string, ext"""
+        if ext is not None and type(ext) != str:
+            raise ValueError('extension must be a string')
         # variable initializing
         self.method = method
         self.folder = folder
@@ -43,7 +46,7 @@ class Search(object):
             return 'No results'
 
     def build_results(self):
-        self.raw_text = '{} seconds\n'.format(self.duration)
+        self.raw_text = 'Search for "{}" in "{}" took {} seconds\n'.format(self.string, self.folder, self.duration)
         self.raw_text += '\n'.join([res + '\n\t' + \
                                     '\n\t'.join([val for val in self.results[res]]) \
                                     for res in list(self.results.keys())])
@@ -52,13 +55,14 @@ class Search(object):
         self.results = _OrderedDict()
         self.matches = 0
         self.last_len = 0
+        self.duration = 0
         walker = _os.walk(self.folder) # root dir
         print('Search in progress...')
         print('Searching: ', end='', flush=True)
         try:
             time_start = _time.time() # start timer
             for root, dirs, files in walker:
-                if any(excl in self.string for excl in EXCLUDED): # if excluded
+                if any(excl in root for excl in EXCLUDED): # if excluded
                     self.print_path('excluded dir ({})'.format(root[:STR_LIM]))
                 else: # not excluded dirs
                     try:
@@ -67,24 +71,30 @@ class Search(object):
                         self.print_path('dir unavailable')
                     for f in files: # search file content
                         flag = False
-                        if self.method.startswith('cont'):
-                            try:
+                        try:
+                            if self.method.startswith('cont'):
+
                                 with open(_os.path.join(root, f), encoding='latin-1') as fp:
                                     text = fp.read()
                                 flag = len(findall(self.string, text)) > 0
-                            except Exception as e:
-                                print(e)
-                        flag = not(flag) and self.method.startswith('name') and findall(self.string, f)
+
+                            elif self.method.startswith('name'):
+                                flag = findall(self.string, f)
+                        except Exception as e:
+                            print() # flush stdout
+                            input(e)
+                            print('Aborting search...')
+                            raise KeyboardInterrupt
                         if flag:
                             self.matches += 1
-                            if self.ext and f.endswith(self.ext) or not self.ext:
-                                if root not in list(self.results.keys()): # if same dir, add a file
-                                    self.results[root] = list([f])
+                            if self.ext is None or len(self.ext) > 0 and f.endswith(self.ext):
+                                if root not in list(self.results.keys()): # create new entry if dir not entered
+                                    self.results[root] = list()
                                 self.results[root].append(f)
 
             self.duration = _time.time() - time_start
         except KeyboardInterrupt:
-            print('KeyboardInterrupt')
+            print('KeyboardInterrupt was raised')
             if not(fp.closed):
                 fp.close()
         print('\nSearch complete')
@@ -119,6 +129,21 @@ def executable_search(string, ext='exe'):
     t = Search('name', folder=r'C:\Program Files (x86)', string=string, ext=ext)
     return dict({s.folder: s.get_results(), t.folder: t.get_results()})
 
+def print_containing_files(string, directory, ext=None):
+    """Given a string, directory(ies), and optional extension,
+       prints the search results for the files containing the
+       string"""
+    if type(directory) == str:
+        directories = [directory]
+    else:
+        directories = directory
+    for d in directories:
+        s = Search('cont', folder=d, string=string, ext=ext)
+        print()
+        print(s)
+        print()
+
 if __name__ == '__main__':
+    import pprint
     s = Search('cont', _os.path.join(_getDocsFolder(), r'Clay\Notes'), 'the')
-    print(executable_search('chrome'))
+    pprint.pprint(executable_search('chrome'))
