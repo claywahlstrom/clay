00abf92c7e43fd02725d3cb37a49ce07e740f08b
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-08-08 23:15:14 -0700

    graphing: Add ability to shorten displayed keys, count numbers

--- graphing.py
+++ graphing.py
@@ -63,6 +63,9 @@ class Histogram(object):
     Always safe to have max_width be one less than the actual screen width.
 
     """
+
+    SHORT_LENGTH = 30
+
     def __init__(self, columns=None, iterable=None,
                  title=None, max_width=SCREEN_WD, sort=True):
         if not columns is None:
@@ -97,35 +100,55 @@ class Histogram(object):
             return '%s()' % (self.__class__.__name__,)
         return '%s(%r)' % (self.__class__.__name__, list(self.sd.items()))
 
-    def build(self):
+    def build(self, limit=0, shorten_keys=False, with_count=False):
         if type(self.cols[0]) == str:
-            largest_key = max(map(len, map(str, list(self.sd.keys()))))
+            longest_key_len = max(map(len, map(str, list(self.sd.keys()))))
         else: # for ints and floats
             # longest str repr of a number
-            largest_key = len(str(max(list(self.sd.keys()))))
-##            width = self.max_width - 1 - len(str(largest_key))
-        if all(x == int(x) for x in self.sd.values()):
+            longest_key_len = len(str(max(list(self.sd.keys()))))
+        base_longest_key_len = longest_key_len
+        if all(type(x) == int for x in self.sd.values()):
             # if integers, just print out one per count
-            width = max(self.sd.values())
+            width = min(max(self.sd.values()), SCREEN_WD)
+            using = 'ints'
         else:
             # otherwise, adjust width for better representation of floats
-            width = self.max_width - largest_key - 1
+            width = self.max_width
+            using = 'floats'
+        if with_count:
+            max_count_len = max(map(len, map(str, list(self.sd.values()))))
+            width -= 3 - max_count_len
+        if longest_key_len > width:
+            print('Using shortened keys')
+            shorten_keys = True
+            longest_key_len = Histogram.SHORT_LENGTH
         max_val = max(list(self.sd.values()))
 
-        print('Histogram for', self.title)
-        print('width', width)
-        print('max val', max_val)
-        for (k, v) in self.sd.items():
-            print('{:>{}}'.format(k, largest_key), end=' ')
+        print('  Histogram for', self.title)
+        print('longest key len', longest_key_len)
+        print('          width', width)
+        print('        max val', max_val)
+        print('          using', using)
+        print('     with count', with_count)
+        shorten_wings = 5 * ((Histogram.SHORT_LENGTH - 3) // 10)
+        for i, (k, v) in enumerate(self.sd.items()):
+            if shorten_keys and len(k) > Histogram.SHORT_LENGTH:
+                k = k[:shorten_wings] + '...' + k[-shorten_wings:]
+            print('{:>{}}'.format(k, longest_key_len), end=' ')
+            if with_count:
+                print('({:>{}})'.format(v, max_count_len), end=' ')
             try:
-                if v <= SCREEN_WD - largest_key - 1:
+                if using == 'ints' and base_longest_key_len < width and \
+                   v <= SCREEN_WD - longest_key_len - 1:
                     print('0' * int(v))
                 else:
                     print('0' * int(v * width / max_val))
             except Exception as e:
                 print(e)
+            if limit != 0 and i >= limit:
+                break
         self.max_val = max_val
-        self.largest_key = largest_key
+        self.longest_key_len = longest_key_len
         self.width = width
 
     def sortbycount(self, reverse=False):
@@ -134,9 +157,9 @@ class Histogram(object):
         self.sd.clear()
         for i,j in enumerate(std):
             self.sd[j[0]] = j[-1]
-    
+
 if __name__ == '__main__':
     s = Histogram(('bc', 'sac', 'aaa'), 'abbcssaaaaaaaaaaacccssacbbcaddsaacc')
     s.build()
-    
+
     graph(test_function, -2, 10, 1.012791487897, roundto=5)
