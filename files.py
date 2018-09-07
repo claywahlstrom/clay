bd1314602090b021b6d72c0f137e0b34d55abbd8
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-09-06 23:09:25 -0700

    Add content watcher, refactor file functions into file object
    Also, rename text replace to replace text to follow verb
    convention for functions.

--- files.py
+++ files.py
@@ -11,28 +11,110 @@ import urllib.request, urllib.error, urllib.parse
 
 from clay.web import WEB_HDRS as _WEB_HDRS
 
-def appendfile(filename, string=str()):
-    """Appends the string to the end of the given file.
-       No new line is preceded"""
-    while not(string):
-        string = input('line: ')
-    with open(filename, 'ab') as fp:
-        try:
-            f.write(string.encode('utf8')) # for str
-        except:
-            f.write(string) # for binary
 
-def countchar(filename, char):
-    """Returns the count occurence of char in filename"""
-    with open(filename) as fp:
-        fcount = fp.read().count(char)
-    return fcount
+class ContentWatcher(object):
 
-def countwords(filename):
-    """Returns the count of words in the given file"""
-    with open(filename) as fp:
-        fcount = len(fp.read().split())
-    return fcount
+    """Class ContentWacther can be used to watch the modification
+       time of a file, which correlates to a change in content"""
+
+    def __init__(self, filename):
+        """Initializes this content watcher with the given file path"""
+        self.set_path(filename)
+
+    def get_mtime(self):
+        """Returns the modified time for the content, returns 0
+           if no file was found"""
+        if _os.path.exists(self.filename):
+            return _os.stat(self.filename).st_mtime
+        print(self.filename, 'does not exist')
+        return 0
+
+    def is_modified(self):
+        """Returns True if the content has changed and resets the
+           modification time, otherwise returns False and does not
+           reset the modification time"""
+        new_time = self.get_mtime()
+        if self.mtime == 0 or new_time != self.mtime:
+            self.mtime = new_time
+            return True
+        return False
+
+    def set_path(self, filename):
+        """Sets the path of the content"""
+        self.filename = filename
+        self.mtime = 0
+
+class File(object):
+
+    """Class File can be used to access a file on the disk"""
+
+    def __init__(self, name):
+        """Initializes this file"""
+        if not(_os.path.exists(name)):
+            raise FileNotFoundError(name)
+        self.name = name
+
+    def append(self, string=''):
+        """Appends the string to the end of this file.
+           No new line is preceded"""
+        while len(string) == 0:
+            string = input('line: ')
+        with open(self.name, 'ab') as fp:
+            try:
+                f.write(string.encode('utf8')) # for non-binary strings
+            except:
+                f.write(string) # for binary strings
+
+    def get_char_count(self, char):
+        """Returns the count occurence of char in this file"""
+        with open(self.name) as fp:
+            fcount = fp.read().count(char)
+        return fcount
+    
+    def get_content(self, binary=False):
+        """Returns the content of this file"""
+        mode = 'r'
+        if binary:
+            mode += 'b'
+        with open(self.name, mode) as fp:
+            fread = fp.read()
+        return fread
+
+    def get_word_count(self):
+        """Returns the count of words in this file"""
+        with open(self.name) as fp:
+            fcount = len(fp.read().split())
+        return fcount
+
+    def parse(self, delim='\n', mode='r'):
+        """Parses this by the given delimiter and returns the resulting list"""
+        with open(self.name, mode) as fp:
+            spl = fp.read().split(delim)
+        return spl
+    
+    def print(self):
+        """Prints the binary contents of this file"""
+        with open(self.name, 'rb') as fp:
+            print(fp.read())
+
+    def switch_lf(self):
+        """Switches the linefeed type from a unix-based machine to
+           Windows and vice versa and overwrites this file"""
+        with open(self.name, 'rb') as fp:
+            fread = fp.read()
+            to_unix = False
+            if b'\r' in fread:
+                to_unix = True
+        with open(self.name, 'wb') as fp:
+            if to_unix:
+                fwrite = fread.replace(b'\r', b'')
+            else:
+                fwrite = fread.replace(b'\n', b'\r\n')
+            fp.write(fwrite)
+        if to_unix:
+            print('Converted to lf')
+        else:
+            print('Converted to crlf')
 
 def fix_quotes(filename):
     """Replaces UTF-8 quotes with ANSI ones"""
@@ -51,17 +133,6 @@ def fix_quotes(filename):
     finally:
         fp.close()
 
-def get_content(filename, binary=False):
-    """Returns file content"""
-    mode = 'r'
-    if binary:
-        mode += 'b'
-    if not(_os.path.exists(filename)):
-        raise FileNotFoundError()
-    with open(filename, mode) as fp:
-        fread = fp.read()
-    return fread
-
 def get_size(name, local=True):
     """Returns the size of the given file or uri.
        Returns 0 if no size was found."""
@@ -78,17 +149,6 @@ def get_size(name, local=True):
         else:
             raise Exception('Basename not found. Need an absolute url path')
     return size
-
-def parsefile(filename, delim='\n', mode='r'):
-    """Parses a file by the given delimiter and returns the resulting list"""
-    with open(filename, mode) as fp:
-        spl = fp.read().split(delim)
-    return spl
-
-def printfile(filename):
-    """Prints the binary contents of the given file"""
-    with open(filename, 'rb') as fp:
-        print(fp.read())
 
 def save(text, name='saved_text.txt', use_epoch=True):
     """Saves the given text to the file with sequential numbering"""
@@ -109,44 +169,10 @@ def _save_helper(SP, x):
     """Returns a possible filename. Helps `save` with finding a
        valid name for a file"""
     return '{}-{:03d}{}'.format(SP[0], x, SP[-1])
-        
-def switch_lf(filename):
-    """Switches the linefeed type from a unix-based machine to
-       Windows and vice versa and overwrites the file"""
-    with open(filename, 'rb') as fp:
-        fread = fp.read()
-        to_unix = False
-        if b'\r' in fread:
-            to_unix = True
-    with open(filename, 'wb') as fp:
-        if to_unix:
-            fwrite = fread.replace(b'\r', b'')
-        else:
-            fwrite = fread.replace(b'\n', b'\r\n')
-        fp.write(fwrite)
-    if to_unix:
-        print('Converted to lf')
-    else:
-        print('Converted to crlf')
 
-def _tr_base(filename, old, new):
-    """Helps `text_replace` rename a particular file
-    http://stackoverflow.com/questions/6648493/open-file-for-both-reading-and-writing"""
-    try:
-        fp = open(filename, 'rb+')
-        fread = fp.read()
-        fp.seek(0)
-        fp.write(fread.replace(old, new))
-        fp.truncate() # add NULL terminator
-        print('text_replace on "{}" complete'.format(filename))
-    except Exception as e:
-        print('Error:', e)
-    finally:
-        fp.close()
-
-def text_replace(name, old, new, recurse=False, ext=str()):
+def replace_text(name, old, new, recurse=False, ext=str()):
     """Replaces `name` with binary string params `old` and `new`"""
-    from clay.files import _tr_base
+    from clay.files import _rt_helper
     if recurse:
         sure = eval(input('Replace all "{1}" in "{0}" with "{2}" (True/False)? '.format(name, old, new)))
 
@@ -156,7 +182,7 @@ def text_replace(name, old, new, recurse=False, ext=str()):
                     try:
                         fp = open(_os.path.join(root, f), 'rb')
                         if old in fp.read() and f.endswith(ext):
-                            _tr_base(_os.path.join(root, f), old, new)
+                            _rt_helper(_os.path.join(root, f), old, new)
                     except Exception as e:
                         raise e
                     finally:
@@ -166,7 +192,22 @@ def text_replace(name, old, new, recurse=False, ext=str()):
             print('Aborted')
 
     else:
-        _tr_base(name, old, new)
+        _rt_helper(name, old, new)
+
+def _rt_helper(filename, old, new):
+    """Helps `replace_text` rename a single file
+       http://stackoverflow.com/questions/6648493/open-file-for-both-reading-and-writing"""
+    try:
+        fp = open(filename, 'rb+')
+        fread = fp.read()
+        fp.seek(0)
+        fp.write(fread.replace(old, new))
+        fp.truncate() # add a NULL terminator
+        print('text_replace on "{}" complete'.format(filename))
+    except Exception as e:
+        print('Error:', e)
+    finally:
+        fp.close()
 
 if __name__ == '__main__':
     print('Expects basename not to exist')
