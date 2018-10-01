3bd8f3af432a63711cd1f8ee005b8ba790d5c709
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-10-01 10:41:56 -0700

    Add test for expectation that fails and include expected and result

--- tests.py
+++ tests.py
@@ -4,12 +4,13 @@ def it(expectation, test_input, test_output, transformer=lambda x: x):
        given test input to the test output. Test output may either
        be a value or function accepting one argument. Resembles
        the `it` function provided in the Jasmine JS testing module."""
-    if transformer(test_input) == test_output or \
-        test_input == test_output:
-        print('Passed:', expectation)
+    result = transformer(test_input)
+    if result == test_output:
+        print(f'Passed: {expectation}')
     else:
-        print('Failed:', expectation)
+        print(f'Failed: {expectation}. Expected {test_output} but found {result}')
 
 if __name__ == '__main__':
     it('returns true for given values', 0, 0)
     it('returns true for len function', [], 0, len)
+    it('returns true for 0 == 0', 0, 1)
