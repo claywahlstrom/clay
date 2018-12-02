7c8b8254cab4b6984b86f62f4cda229b52ba2ed4
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2018-12-01 20:28:54 -0800

    Update prefix-unit to include scientific exponent
    Also, decrease access to constants in physics to improve
    module docs formatting.

--- physics.py
+++ physics.py
@@ -7,14 +7,15 @@ physics: goodies ranging from constants to mechanics to electromagnetism
 import collections as _collections
 import math as _math
 
-from clay.constants import *
+from clay import constants as const
 
 OHMS_TABLE = {'I': 'V/R',
               'V': 'I*R',
               'R': 'V/I'}
 # Standard SI prefixes
+NO_PREFIX = ''
 PREFIXES = ['Yotta', 'Zetta', 'Exa', 'Peta', 'Tera', 'Giga',
-            'Mega', 'kilo', '', 'milli', 'micro(u)', 'nano', # '' is the base
+            'Mega', 'kilo', NO_PREFIX, 'milli', 'micro(u)', 'nano',
             'pico', 'femto', 'atto', 'zepto', 'yocto']
 
 def apply_ohms_law(V=None, I=None, R=None):
@@ -35,7 +36,7 @@ def capacitor_energy(charge, distance, area):
 class Charge(object):
 
     """Class Charge can be used to represent a point charge"""
-    
+
     def __init__(self, position, magnitude):
         self.position  = position
         self.magnitude = magnitude
@@ -48,9 +49,9 @@ def dipole_moment(charge, a, l):
            charge = charge of the particle in Coulombs
            a      = distance to the perpendicular bisector in meters
            l      = distance between charges in meters"""
-    return 1 / (4 * _math.pi * EPSILON_0) * charge / (a ** 2 + l ** 2) ** (3/2)
+    return 1 / (4 * _math.pi * const.EPSILON_0) * charge / (a ** 2 + l ** 2) ** (3/2)
 
-def drop_time(displacement, v_i=0, a=ACCEL_GRAV):
+def drop_time(displacement, v_i=0, a=const.ACCEL_GRAV):
     """Returns free fall time in seconds starting at t = 0.
        Assumes up as the positive direction for position"""
     from clay.maths import roots
@@ -58,7 +59,7 @@ def drop_time(displacement, v_i=0, a=ACCEL_GRAV):
         t1, t2 = roots(a, v_i, displacement)
         return max(t1, t2)
     else:
-        return _math.sqrt(displacement * 2 / ACCEL_GRAV)
+        return _math.sqrt(displacement * 2 / const.ACCEL_GRAV)
 
 class Force(object):
 
@@ -70,7 +71,7 @@ class Force(object):
 
     def __repr__(self):
         return 'Force(x=%r,y=%r)' % (self.comp(_math.cos), self.comp(_math.sin))
-        
+
     def mag(self):
         """Returns the magnitude of this force object"""
         return -k * self.one.magnitude * self.two.magnitude / (self.one.position[0] - self.two.position[0]) ** 2
@@ -84,32 +85,35 @@ class Force(object):
 
 def lorenz_factor(velocity):
     """Returns the Lorenz factor for the given object moving at velocity v"""
-    return 1 / _math.sqrt(1 - velocity ** 2 / SPEED_OF_LIGHT ** 2)
-    
-def prefix_unit(scalar, units='m'):
+    return 1 / _math.sqrt(1 - velocity ** 2 / const.SPEED_OF_LIGHT ** 2)
+
+def prefix_unit(scalar, units):
     """Returns the conversion of the given to the appropriate prefix"""
     negative = False
     amount = scalar
     if amount < 0:
         negative = True
         amount *= -1
-    position = PREFIXES.index('')
-    pre = ''
+    neutral_index = PREFIXES.index(NO_PREFIX)
+    position = neutral_index
 
     while amount > 1000:
         position -= 1
-        pre = PREFIXES[position]
         amount /= 1000
 
     while amount < 1:
         position += 1
-        pre = PREFIXES[position]
         amount *= 1000
-        
+
     if negative:
         amount *= -1
-    
-    return '{} {} => {} {} {}'.format(scalar, units, amount, pre, units)
+
+    pre = PREFIXES[position]
+
+    prefix_fmt = '{} {} <=> {}'.format(scalar, units, amount)
+    if position != neutral_index:
+        prefix_fmt += ' {} (e{})'.format(pre, 3 * (neutral_index - position))
+    return prefix_fmt + ' ' + units
 
 class Position(object):
 
@@ -123,7 +127,7 @@ class Position(object):
         self.step = step
 
         if self.time:
-            self.dt = [self.time[t+1]-self.time[t] for t in range(len(self.time)-1)]            
+            self.dt = [self.time[t+1]-self.time[t] for t in range(len(self.time)-1)]
         else:
             self.time = list(range(0, len(position), step))
             self.dt = list([step]*(len(self.position)-1))
@@ -153,10 +157,10 @@ def requivalent(res, config):
 def urms(temp, molar_mass):
     """"Returns the root mean square velocity using the given molar mass (g)
         and temperature (*C)"""
-    return _math.sqrt(3 * RYDBERG_ENERGY * (temp + KELVIN_OFFSET) / (molar_mass / 1000))
+    return _math.sqrt(3 * const.RYDBERG_ENERGY * (temp + const.KELVIN_OFFSET) / (molar_mass / 1000))
 
 if __name__ == '__main__':
-    print('drop time', drop_time(displacement=abs(ACCEL_GRAV / 2), v_i=0))
+    print('drop time', drop_time(displacement=abs(const.ACCEL_GRAV / 2), v_i=0))
     pos = Position([0, 4.905, 19.62, 44.145, 78.48, 122.625])
     print('vel =', pos.vel())
     print('accel =', pos.accel())
@@ -164,9 +168,10 @@ if __name__ == '__main__':
     POS = Position([1, 4, 9, 16, 25, 36])
     print('velocities', POS.vel())
     print('accels', POS.accel())
-    print(prefix_unit(24582000))
-    print(prefix_unit(0.0021040, units='m/s'))
-    print(prefix_unit(0.00021040))
+    print(prefix_unit(100, 's'))
+    print(prefix_unit(24582000, 'm'))
+    print(prefix_unit(0.0021040, 'm/s'))
+    print(prefix_unit(0.00021040, 'm/s'))
     print('requivalent', requivalent([10, requivalent([100, 25, 100, 50, 12.5],
                                                       'parallel')],
                                      'series'))
