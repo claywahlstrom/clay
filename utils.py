3d1b9d328721113941b18e1c135c32ec674f692b
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2019-12-07 14:04:08 -0800

    Update utils to use tests framework

--- utils.py
+++ utils.py
@@ -30,11 +30,8 @@ class Anonymous(object):
             setattr(self, key, kwargs[key])
 
     def __repr__(self):
-        if not self:
-            return r'Anonymous()'
         return r'Anonymous({})'.format(
-            ', '.join(name + '=' + str(getattr(self, name)) for name in self.get_attributes())
-        )
+            ', '.join(name + '=' + str(getattr(self, name)) for name in self.get_attributes()))
 
     def get_attributes(self):
         attrs = _inspect.getmembers(self, lambda a:not(_inspect.isroutine(a)))
@@ -57,7 +54,7 @@ class Linq(object):
     __select_called = False
 
     def __check_queryable_exists():
-        if not(Linq.__queryable_set):
+        if not Linq.__queryable_set:
             raise RuntimeError('Linq queryable must be set')
 
     def __check_distinct_projection(distinct):
@@ -184,7 +181,8 @@ class Watch(object):
        in the debugging phase of a project."""
 
     def __init__(self, objs=[], module='__main__'):
-        assert type(objs) == list, 'Not a list of strings'
+        if type(objs) != list or any(type(obj) != str for obj in objs):
+            raise TypeError('objs must be a list of strings')
 
         self.objs = objs
         self.module = module
@@ -199,8 +197,7 @@ class Watch(object):
         if type(var) != str:
             raise TypeError('var must be the string name of the object')
         if callable(self.get_dict()[var]):
-            print('callable types cannot be added to this watch')
-            return
+            raise TypeError('callable types cannot be added to this watch')
 
         self.objs.append(var)
 
@@ -251,79 +248,108 @@ class Watch(object):
 
 if __name__ == '__main__':
 
-    import traceback
-    import pprint
-    from random import randint
-    import sys
+    from clay.tests import testif
 
     objs = [Anonymous(a=1), Anonymous(a=2, b=3), Anonymous(a=2, b=1)]
 
-    print(Linq.query(objs).where(lambda x: x.a == 2).first_or_default())
-    print(Linq.query([['John', 'Smith', '1/1/2000']]).select([0, 6, 2]))
+    testif('Linq first or default selects correct element',
+        Linq.query(objs).where(lambda x: x.a == 2).first_or_default(),
+        objs[1])
+    testif('Linq select selects data from indices',
+        Linq.query([['John', 'Smith', '1/1/2000']]).select([0, 6, 2]).to_list(),
+        [['John', '1/1/2000']])
 
     test_queryable = [{'num': 1}, {'num': 2}, {'num': 2}, {'num': 3}]
 
-    try:
-        Linq.where(lambda x: True)
-    except Exception as e:
-        print(e)
-    try:
-        Linq.query(test_queryable).select('num').where(lambda x: x['num'] == 2)
-    except Exception as e:
-        print('where is after select:', e)
-    try:
-        print(Linq.query(test_queryable).where(lambda x: x['num'] == 2).select('num'))
-    except Exception as e:
-        print(e)
+    testif('Linq raises RuntimeError when no queryable specified',
+        lambda: Linq.where(lambda x: True),
+        None,
+        raises=RuntimeError)
+    testif('Linq raises RuntimeError when filtering after select',
+        lambda: Linq.query(test_queryable).select('num').where(lambda x: x['num'] == 2),
+        None,
+        raises=RuntimeWarning)
+    testif('Linq query selects property correctly',
+        Linq.query(test_queryable).where(lambda x: x['num'] == 2).select('num').to_list(),
+        [2, 2])
 
-    print('human hex for 2700 is', human_hex(2700))
+    testif('human_hex converts integer 2700 correctly', human_hex(2700), 'a8c')
+
     array = (1, 4, 16, 25)
-    print('map args for', array, '->', map_args(_map_args_test, array, z = 4))
+    testif('map_args returns correct type',
+        type(map_args(_map_args_test, array, z = 4)),
+        type(array))
+    testif('map_args returns correct values',
+        map_args(_map_args_test, array, z = 4),
+        (7, 10, 22, 31))
 
     obj = Anonymous({
         'one': 1,
         'two': 2,
         'three': 3
     })
-    print('prints', obj.one, '-> expects 1')
-    print('prints', obj.two, '-> expects 2')
-    print('prints', obj.three, '-> expects 3')
-    print('get attrs prints', len(obj.get_attributes()) == 3, '-> expects True')
+    testif('Anonymous sets attribute correctly (1)', obj.one, 1)
+    testif('Anonymous sets attribute correctly (2)', obj.two, 2)
+    testif('Anonymous sets attribute correctly (3)', obj.three, 3)
+    testif('Anonymous has three attributes set', len(obj.get_attributes()), 3)
 
-    person = {'friends': [{'id': 0, 'name': 'Carla James'},
+    person = {
+        'friends': [{'id': 0, 'name': 'Carla James'},
                           {'id': 1, 'name': 'Patel Lewis'},
                           {'id': 2, 'name': 'Lacey Brady'}],
-              'isActive': True, 'name': 'Celia Vaughan',
-              'gender': 'female', 'age': 36,
-              'greeting': 'Hello, Celia Vaughan! You have 7 unread messages.',
-              'longitude': -69.800663, 'balance': '$3,701.90'}
+        'isActive': True,
+        'name': 'Celia Vaughan',
+        'gender': 'female',
+        'age': 36,
+        'greeting': 'Hello, Celia Vaughan! You have 7 unread messages.',
+        'longitude': -69.800663,
+        'balance': '$3,701.90'}
 
-    print('before sort')
-    print(person)
+    person_expected = {
+        'age': 36,
+        'balance': '$3,701.90',
+        'friends': [{'id': 0, 'name': 'Carla James'},
+                          {'id': 1, 'name': 'Patel Lewis'},
+                          {'id': 2, 'name': 'Lacey Brady'}],
+        'gender': 'female',
+        'greeting': 'Hello, Celia Vaughan! You have 7 unread messages.',
+        'isActive': True,
+        'longitude': -69.800663,
+        'name': 'Celia Vaughan'}
+
     celia = SortableDict(person)
     celia.sort()
-    print('after sort')
-    pprint.pprint(celia)
+    testif('SortableDict sorts iterable correctly', celia, person_expected)
 
     a = 'string'
     b = int(4)
     c = {}
-    s = Watch(['a', 'b', 'c'])
-    print('is watching d should be False =>', s.is_watching('d'))
+    testif('Watch raises TypeError for incorrect objs type',
+        lambda: Watch({'invalid': 'type'}),
+        None,
+        raises=TypeError)
+    testif('Watch raises TypeError for incorrect obj type',
+        lambda: Watch([None, 'valid_name', 123]),
+        None,
+        raises=TypeError)
+    w = Watch(['a', 'b', 'c'])
+    testif('Watch is not watching d', w.is_watching('d'), False)
     print('before add')
-    s.view()
+    w.view()
     d = float(542.2)
-    s.add('d')
-    print('is watching d should be True =>', s.is_watching('d'))
+    w.add('d')
+    testif('Watch is watching d', w.is_watching('d'), True)
     print('after add')
-    s.view()
-    s.write_file(r'test_files\watch_test.txt')
+    w.view()
+    w.write_file(r'test_files\watch_test.txt')
 
+    testif('Watch raises TypeError when adding non-string type',
+        lambda: w.add(None),
+        None,
+        raises=TypeError)
     def test_function(x):
         return x
-    s.add('test_function')
-    print('The next Watch test should throw an exception')
-    try:
-        s.add(None)
-    except Exception:
-        traceback.print_exception(*sys.exc_info())
+    testif('Watch raises TypeError when adding callable string type',
+        lambda: w.add('test_function'),
+        None,
+        raises=TypeError)
