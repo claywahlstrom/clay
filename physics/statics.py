382f2aff458cd2b922035001aa00ee30e00a7410
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2020-01-05 03:22:45 -0800

    Update physics statics to use tests

--- physics/statics.py
+++ physics/statics.py
@@ -1,30 +1,34 @@
 
 """
-
 Module used for analyzing objects and systems in static equilibrium
-
 
 """
 
 from clay.maths.core import round_qty as _r
 
 def id_member(name, value):
-    """Given the member name and value, prints the axial force type
-       and returns its magnitude. Assumes the initial direction of the
-       reaction force points away from the joint."""
+    """Given the member name and value, returns the member name,
+       value, and axial force type identified as a string. Assumes
+       the initial direction of the reaction force points away from
+       the joint."""
     if value < 0:
         stress_type = 'C'
     elif value == 0:
         stress_type = '0'
     else: # value > 0
         stress_type = 'T'
-    print('member {}: {} ({})'.format(name, _r(value), stress_type))
-    return value
+    return 'member {}: {} ({})'.format(name, _r(value), stress_type)
 
 if __name__ == '__main__':
 
-    # TESTS
-    id_member('TAB', -1)
-    id_member('TBC', 0)
-    id_member('TCD', 1)
-    
+    from clay.tests import testif
+
+    testif('id_member identifies compression correctly',
+        id_member('TAB', -1),
+        'member TAB: -1 (C)')
+    testif('id_member identifies no external loads correctly',
+        id_member('TBC', 0),
+        'member TBC: 0 (0)')
+    testif('id_member identifies tension correctly',
+        id_member('TCD', 1),
+        'member TCD: 1 (T)')
