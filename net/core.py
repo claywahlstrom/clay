a25e5b8caa88fe75a3e3adc7e211e697e058b06a
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2020-02-22 21:50:16 -0800

    Add support to URL builder for path segments and query strings

--- net/core.py
+++ net/core.py
@@ -38,6 +38,7 @@ for n in LINK_SIZES:
 del n
 LINKS['1GB'] = 'http://download.thinkbroadband.com/1GB.zip'
 
+EXAMPLE_URL = 'http://example.com'
 TEST_LINK = 'https://minecraft.net/en-us/'
 VALID_SCHEMES = ('ftp', 'http', 'https')
 WEB_HDRS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
@@ -409,21 +410,53 @@ class TagFinder(object):
             print('TagFinder store failed: {}'.format(e))
 
 class UrlBuilder(object):
+    """Can be used to build formatted URLs"""
 
-    def __init__(self, base):
-        self.base_url = base
-        self.url = base
+    def __init__(self, base_url):
+        """Initializes this UrlBuilder"""
+        self.base_url = base_url
+        self.reset()
 
-    def with_query_params(self, params):
-        if '?' in self.url: # already exists
-            self.url += '&'
-        else: # does not exist
-            self.url += '?'
-        self.url += urllib.parse.urlencode(params)
+    def __repr__(self):
+        """Returns the string representation of this UrlBuilder"""
+        return 'UrlBuilder(base_url={},\n'.format(self.base_url) \
+            + ' ' * 4 + 'url={})'.format(self.__url)
+
+    def append_segments(self, *paths):
+        """Appends the given path segments to the URL"""
+        # if joining the URL with the first path segment yeilds two '/'s
+        if self.__url.endswith('/') and paths[0].startswith('/'):
+            paths = list(paths) # allow for mutability
+            paths[0] = paths[0][1:] # remove the / not part of the base URL
+        # if joining the URL or the first path segment yeilds no '/'s
+        elif not self.__url.endswith('/') and not paths[0].startswith('/'):
+            self.__url += '/' # add one /
+        self.__url += '/'.join(paths)
         return self
 
+    def with_query_params(self, params):
+        """Adds the given query params key-value pairs to the URL"""
+        return self.with_query_string(urllib.parse.urlencode(params))
+
+    def with_query_string(self, string):
+        """Adds the given query string to the URL"""
+        # if URL is the same as the base and only scheme separators exist
+        if self.__url == self.base_url and self.__url.count('/') == 2:
+            # suffix with / before adding query params
+            self.__url += '/'
+        # add separator if query params already exist,
+        # otherwise add query params start symbol
+        self.__url += '&' if '?' in self.__url else '?'
+        self.__url += string
+        return self
+
+    def reset(self):
+        """Resets the URL to the base URL"""
+        self.__url = self.base_url
+
     def build(self):
-        return self.url
+        """Builds the URL string for this UrlBuilder"""
+        return self.__url
 
 class WeatherPollenApiUrlBuilder(UrlBuilder):
 
@@ -434,8 +467,8 @@ class WeatherPollenApiUrlBuilder(UrlBuilder):
             'language': 'en-US'}).build()
 
     def with_geocode(self, lat, lon):
-        self.url = self.base_url + '&' + urllib.parse.urlencode({'geocode': str(lat) + ',' + str(lon)})
-        return self
+        """Adds the geocode latitude and longitude param to the URL"""
+        return self.reset() or self.with_query_params({'geocode': str(lat) + ',' + str(lon)})
 
 class WebDocument(object):
 
@@ -900,6 +933,24 @@ if __name__ == '__main__':
     we2.find('a')
     with open('logs/net-core-tagfinder.log', 'w') as tf_log:
         we2.show(attribute='href', file=tf_log)
+
+    print()
+    expected_url = 'http://example.com/?key=value'
+    for test_url in ('http://example.com/', EXAMPLE_URL):
+        url_builder = UrlBuilder(test_url)
+        url_builder.with_query_string('key=value')
+        testif('UrlBuilder adds query string correctly', url_builder.build(), expected_url)
+    expected_url = 'http://example.com/path/segment'
+    for test_url in ('http://example.com/', EXAMPLE_URL):
+        url_builder = UrlBuilder(test_url)
+        for test_path in ('path', '/path'):
+            url_builder.reset()
+            url_builder.append_segments(test_path, 'segment')
+            testif('UrlBuilder adds path segments correctly', url_builder.build(), expected_url)
+    print(url_builder) # test the string representation
+    testif('UrlBuilder resets URL to base correctly',
+        url_builder.reset() or url_builder.build(),
+        EXAMPLE_URL)
 
     print()
     testif('webdoc sets uri correctly for empty uri',
