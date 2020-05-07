99cd88faa6ea7cface63cec7b00e4147c528f338
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2020-05-06 17:41:48 -0700

    Install dependencies automatically

--- install.bat
+++ install.bat
@@ -3,6 +3,9 @@ set %destination%=C:\Python37\Lib\site-packages\clay
 robocopy . %destination% /mir
 if exist %destination%\README.md (
     title Install complete
+    title Installing dependencies...
+    pip install bs4 requests
+    title Done
     cd %destination%
 ) else (
     title Install failed
