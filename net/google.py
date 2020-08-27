41f11208bda114690e44ed6d8167f76fecf2b218
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2020-08-27 08:58:07 -0700

    Add docs method to GoogleSuite object
    Allow opening of local Google Docs(TM) files on the web for viewing
    or editing.

--- net/google.py
+++ net/google.py
@@ -4,14 +4,18 @@ google: open common Google(TM) apps using Python
 
 """
 
+import json as _json
+import os as _os
 from urllib.parse import urlencode as _urlencode
 
-from clay.net.core import DEFAULT_BROWSER
+from clay.net.core import DEFAULT_BROWSER, WebDocument
+
+_gs_web_doc = WebDocument()
 
 def launch_gs_app(name, browser, url):
-    from subprocess import call
     print('Opening {} in {}...'.format(name, browser), end=' ')
-    call('start {} "{}"'.format(browser, url), shell=True)
+    _gs_web_doc.set_uri(url)
+    _gs_web_doc.launch(browser)
     print('Done')
 
 class GoogleSuite(object):
@@ -23,6 +27,15 @@ class GoogleSuite(object):
 
     def calendar(self):
         launch_gs_app('calendar', self.browser, 'https://calendar.google.com')
+
+    def docs(self, path):
+        with open(path) as fp:
+            content = _json.load(fp)
+
+        if 'url' not in content:
+            raise RuntimeError('url not in content')
+
+        launch_gs_app('docs', self.browser, content['url'])
 
     def drive(self):
         launch_gs_app('drive', self.browser, 'https://drive.google.com')
