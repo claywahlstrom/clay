7152eeeac6471ec4f08db6ebceea152b87ad0cb4
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2020-12-11 16:22:17 -0800

    Add HTML input element markup helpers to net vial Html class
    Allow generation of markup via input element helpers for use in rendered
    templates.

--- net/vial.py
+++ net/vial.py
@@ -52,6 +52,43 @@ class Html(clay.models.Static):
         text = text if text is not None else name
         return '<label for="{}">{}</label>'.format(name, text)
 
+    @staticmethod
+    def input(name, type, value='', attrs={}):
+        """Returns markup for the input element `name` of type `type`,
+        value `value, and attributes `attrs`"""
+        attribs = ''.join('{}="{}" '.format(key, value) for key, value in attrs.items())
+        return '<input type="{0}" id="{1}" name="{1}" value="{2}" {3}/>' \
+            .format(type, name, value, attribs)
+
+    @staticmethod
+    def checkbox(name, checked=False):
+        """Returns markup for the checkbox input element `name`"""
+        attrs = {'checked': 'checked'} if checked else {}
+        return Html.input(name, 'checkbox', attrs=attrs)
+
+    @staticmethod
+    def date(name, value='', attrs={}):
+        """Returns markup for the date input element `name` of value `value`,
+        and attributes `attrs`"""
+        return Html.input(name, 'date', value=value, attrs=attrs)
+
+    @staticmethod
+    def hidden(name, value=''):
+        """Returns markup for the hidden input element `name` of value `value`"""
+        return Html.input(name, 'hidden', value=value)
+
+    @staticmethod
+    def submit(value='', attrs={}):
+        """Returns markup for the submit input element of value `value` and attributes `attrs`"""
+        attribs = ''.join('{}="{}" '.format(key, value) for key, value in attrs.items())
+        return '<input type="submit" class="btnSubmit" value="{0}" {1}/>'.format(value or 'Submit', attribs)
+
+    @staticmethod
+    def text(name, value='', type='text', attrs={}):
+        """Returns markup for the text input element of name `name`, value `value`,
+        and attributes `attrs`"""
+        return Html.input(name, type, value=value, attrs=attrs)
+
 def json_code_result(status_code, message=''):
     """Returns a `flask.Response` object with the given status code and message"""
     return flask.jsonify(hasMessage=bool(message),
@@ -72,13 +109,13 @@ if __name__ == '__main__':
     from clay.tests import testif
     from clay.utils import qualify
 
-    testif('Raises TypeError for invalid string type',
+    testif('raises TypeError for invalid string type',
         lambda: Html.get_newlines_string(None),
         None,
         raises=TypeError,
         name=qualify(Html.get_newlines_string))
 
-    testif('Accepts string of type bytes',
+    testif('accepts string of type bytes',
         Html.get_newlines_string(b''),
         b'',
         name=qualify(Html.get_newlines_string))
@@ -89,20 +126,62 @@ if __name__ == '__main__':
     ]
 
     for test in html_get_newlines_string_tests:
-        testif('Returns correct HTML newlines string when contains carriage return (type: {})'
+        testif('returns correct HTML newlines string when contains carriage return (type: {})'
                 .format(type(test[0]).__name__),
             Html.get_newlines_string(test[0]),
             test[1],
             name=qualify(Html.get_newlines_string))
 
-    testif('Returns correct HTML newlines string',
+    testif('returns correct HTML newlines string',
         Html.get_newlines_string('First line\nSecond line'),
         'First line<br />Second line',
         name=qualify(Html.get_newlines_string))
 
     test_md_str_input = 'Test: Insert link <a target="_blank" href="http://www.comcast.net">here~^</a> [link2](http://www.google.com) and [link3](http://youtube.com)'
     test_md_str_output = 'Test: Insert link <a target="_blank" href="http://www.comcast.net">here~^</a> <a target="_blank" href="http://www.google.com">link2~^</a> and <a target="_blank" href="http://youtube.com">link3~^</a>'
-    testif('Replaces md links correctly',
+    testif('replaces md links correctly',
         Html.replace_md_with_anchor(test_md_str_input),
         test_md_str_output,
         name=qualify(Html.replace_md_with_anchor))
+
+    testif('returns correct HTML string',
+        Html.input('tests', 'text'),
+        '<input type="text" id="tests" name="tests" value="" />',
+        name=qualify(Html.input))
+
+    testif('returns correct HTML string (not checked)',
+        Html.checkbox('hasTests'),
+        '<input type="checkbox" id="hasTests" name="hasTests" value="" />',
+        name=qualify(Html.checkbox))
+    testif('returns correct HTML string (checked)',
+        Html.checkbox('hasTests', checked=True),
+        '<input type="checkbox" id="hasTests" name="hasTests" value="" checked="checked" />',
+        name=qualify(Html.checkbox))
+
+    testif('returns correct HTML string',
+        Html.date('testDate', '2020-12-11'),
+        '<input type="date" id="testDate" name="testDate" value="2020-12-11" />',
+        name=qualify(Html.date))
+
+    testif('returns correct HTML string',
+        Html.hidden('testId', '01234'),
+        '<input type="hidden" id="testId" name="testId" value="01234" />',
+        name=qualify(Html.hidden))
+
+    testif('returns correct HTML string (no value)',
+        Html.submit(),
+        '<input type="submit" class="btnSubmit" value="Submit" />',
+        name=qualify(Html.submit))
+    testif('returns correct HTML string (with value)',
+        Html.submit('Save'),
+        '<input type="submit" class="btnSubmit" value="Save" />',
+        name=qualify(Html.submit))
+    testif('returns correct HTML string (with attrs)',
+        Html.submit(attrs={'id': 'btnSubmitChanges', 'style': 'display: none'}),
+        '<input type="submit" class="btnSubmit" value="Submit" id="btnSubmitChanges" style="display: none" />',
+        name=qualify(Html.submit))
+
+    testif('returns correct HTML string',
+        Html.text('testName'),
+        '<input type="text" id="testName" name="testName" value="" />',
+        name=qualify(Html.text))
