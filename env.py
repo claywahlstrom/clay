da2954116b563304da5c0f09a2fae821b14dbd46
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2021-03-14 12:09:08 -0700

    Determine powershell environment by larger PS module path
    Add a win32 environment helper function to ensure powershell check is
    made only on win32 machines. Improve powershell env detection.

--- env.py
+++ env.py
@@ -4,6 +4,7 @@ Runtime environment settings
 
 """
 
+import os as _os
 import sys as _sys
 
 def is_idle():
@@ -12,7 +13,8 @@ def is_idle():
 
 def is_powershell():
     """Returns True if the script is running within PowerShell, False otherwise"""
-    return not is_idle() and not is_launcher()
+    # per mklement0 via https://stackoverflow.com/a/55598796/5645103
+    return is_win32() and len(_os.getenv('PSModulePath', '').split(_os.pathsep)) >= 3
 
 def is_launcher():
     """
@@ -29,3 +31,7 @@ def is_posix():
 
     """
     return any(_sys.platform.startswith(x) for x in ('linux', 'darwin')) # darwin for macOS
+
+def is_win32():
+    """Returns True if the script is running within a win32 machine, False otherwise"""
+    return _sys.platform == 'win32'
