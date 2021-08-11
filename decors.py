03d2a5af9537bd3c913e116fca3b1c60d53ce131
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2021-08-10 20:18:53 -0700

    Add type annotations to decors module to improve usability

--- decors.py
+++ decors.py
@@ -4,16 +4,18 @@ Decorators
 
 """
 
+from collections import abc
+
 from clay.utils import qualify
 
-def obsolete(func):
+def obsolete(func: abc.Callable) -> abc.Callable:
     """Decorator for warning the consumer the function is obsolete"""
     def inner(*args, **kwargs):
         print('Function {} is obsolete'.format(qualify(func)))
         return func(*args, **kwargs)
     return inner
 
-def vintage(func):
+def vintage(func: abc.Callable) -> abc.Callable:
     """Decorator for warning the consumer the function is vintage"""
     def inner(*args, **kwargs):
         print('Function {} is vintage'.format(qualify(func)))
