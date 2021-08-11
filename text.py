7bde70d40a2fc6d7825b8fce0fbd9a82530bf157
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2021-08-10 21:18:51 -0700

    Add type annotations to text module to improve usability

--- text.py
+++ text.py
@@ -6,6 +6,7 @@ Default WIDTH is 80 characters for IDLE and 100 for shell.
 
 """
 
+from collections import abc
 import pprint
 
 from clay.env import is_idle as _is_idle
@@ -15,7 +16,7 @@ if _is_idle():
 else:
     from clay.settings import CONSOLE_WIDTH as WIDTH
 
-def box(text, width=0, height=3, module=False):
+def box(text: str, width: int=0, height: int=3, for_module: bool=False) -> None:
     """
     Prints a formatted box based on size of text w/ thickness of 1.
     Optional module headers to customize titles
@@ -26,7 +27,7 @@ def box(text, width=0, height=3, module=False):
             if len(line) + 4 > width:
                 width = len(line) + 4
     assert height >= 3
-    if module:
+    if for_module:
         height += 2
 
     border = 1
@@ -46,11 +47,11 @@ def box(text, width=0, height=3, module=False):
 
     print('#' * width)
 
-def center(text):
+def center(text: str) -> str:
     """Justifies the given text to the center"""
     return text.center(WIDTH)
 
-def fullbox(text, thickness=1, border=1):
+def fullbox(text: str, thickness: int=1, border: int=1) -> None:
     """Prints a formatted box based on "thickness" and "border" around text"""
     width = len(text) + thickness * 2 + border * 2
 
@@ -68,24 +69,24 @@ def fullbox(text, thickness=1, border=1):
     for _ in range(thickness):
         print('#' * width)
 
-def _fullbox_printline(text, thickness):
+def _fullbox_printline(text: str, thickness: int) -> None:
     """A helper for fullbox"""
     print('#' * thickness + text + '#' * thickness)
 
-def left(text):
+def left(text: str) -> str:
     """Justifies the given text to the left"""
     return text
 
-def right(text):
+def right(text: str) -> str:
     """Justifies the given text to the right"""
     return text.rjust(WIDTH)
 
-def is_capitalized(text):
+def is_capitalized(text: str) -> str:
     """Returns True if the given text is capitalized, False otherwise"""
     if not text: return False
     return text[0].isupper()
 
-def uncapitalize(text):
+def uncapitalize(text: str) -> str:
     """Returns the given text uncapitalized"""
     # return early if empty string or none
     if not text: return ''
@@ -99,23 +100,23 @@ def uncapitalize(text):
     else:
         return text[0].lower() + text[1:]
 
-def underline(text, char='-'):
+def underline(text: str, char: str='-') -> str:
     """Returns the given text underlined with char"""
     return text + '\n' + char * len(text)
 
-def pretty_print(heading, data):
+def pretty_print(heading: str, data: object) -> None:
     """Pretty prints the heading and data"""
     print(underline(heading))
     pprint.pprint(data)
 
-def remove_padding(string):
+def remove_padding(string: str) -> str:
     """Returns the given string stripped and with double-spaces as single-spaces"""
     string = string.strip()
     while '  ' in string:
         string = string.replace('  ', ' ')
     return string
 
-def remove_padding_many(strings):
+def remove_padding_many(strings: abc.Iterable) -> abc.Iterable:
     """
     Returns the given iterable of strings stripped and with double-spaces as single-spaces
 
@@ -130,7 +131,7 @@ if __name__ == '__main__':
     print('Box Art Examples:')
     fullbox('Hello full', 2, 1)
     print('')
-    box('indexops\n---------\nregex for\nsearching\ndirectories', module=True)
+    box('indexops\n---------\nregex for\nsearching\ndirectories', for_module=True)
 
     print(left('hello'))
     print(center('world'))
@@ -139,26 +140,28 @@ if __name__ == '__main__':
     print(underline('underlined text (-)'))
     print(underline('underlined text (*)', '*'))
 
-    testif('Returns False if text less than two characters',
+    testif('returns False if text less than two characters',
         is_capitalized(''),
-        False)
-    testif('Returns True if first character capitalized',
+        False,
+        name=qualify(is_capitalized))
+    testif('returns True if first character capitalized',
         is_capitalized('Hi'),
-        True)
+        True,
+        name=qualify(is_capitalized))
 
-    testif('Returns empty string if text empty',
+    testif('returns empty string if text empty',
         uncapitalize(''),
         '',
         name=qualify(uncapitalize))
-    testif('Returns uncapitalized text (1 character)',
+    testif('returns uncapitalized text (1 character)',
         uncapitalize('H'),
         'h',
         name=qualify(uncapitalize))
-    testif('Returns uncapitalized text (>1 character)',
+    testif('returns uncapitalized text (>1 character)',
         uncapitalize('Hello'),
         'hello',
         name=qualify(uncapitalize))
-    testif('Returns text as-is (uppercase word)',
+    testif('returns text as-is (uppercase word)',
         uncapitalize('HELLO world'),
         'HELLO world',
         name=qualify(uncapitalize))
