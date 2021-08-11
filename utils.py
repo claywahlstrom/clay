dd266847bd0473e03200465b0f45a3b8b4e5259b
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2021-08-10 21:28:02 -0700

    Add type annotations to utils module to improve usability

--- utils.py
+++ utils.py
@@ -4,10 +4,11 @@ Utilities for Python
 
 """
 
+from collections import abc as _abc
 import collections as _collections
 import sys as _sys
 
-def human_hex(dec):
+def human_hex(dec: int) -> hex:
     """
     Converts decimal values to human readable hex.
     Mainly used in engineering class
@@ -15,7 +16,10 @@ def human_hex(dec):
     """
     return hex(dec)[2:]
 
-def map_args(function, iterable, *args, **kwargs):
+def map_args(function: _abc.Callable,
+        iterable: _abc.Iterable,
+        *args,
+        **kwargs) -> _abc.Iterable:
     """
     Maps iterable to a function with arguments/keywords.
     Dynamic types can be used
@@ -23,11 +27,11 @@ def map_args(function, iterable, *args, **kwargs):
     """
     return type(iterable)(function(x, *args, **kwargs) for x in iterable)
 
-def _map_args_test(x, y=2, z=3):
+def _map_args_test(x: float, y: float=2, z: float=3) -> float:
     """A test function for the map_args function. Returns the sum of x, y, z"""
     return x + y + z
 
-def qualify(obj) -> str:
+def qualify(obj: object) -> str:
     """Returns the qualified name of this object"""
     # if a function or bound method
     if hasattr(obj, '__qualname__'):
@@ -43,7 +47,10 @@ def qualify(obj) -> str:
 
 class SortableDict(_collections.OrderedDict):
     """Sortable dict, child of collections.OrderedDict"""
-    def sort(self, key=None, reverse=False, debug=False):
+    def sort(self,
+            key: _abc.Callable=None,
+            reverse: bool=False,
+            debug: bool=False) -> None:
         """Sorts this dict"""
         part = list(self.keys()) # extract keys
         part.sort(key=key, reverse=reverse) # sorts keys
@@ -61,7 +68,7 @@ class Watch(object):
 
     """
 
-    def __init__(self, objs=[], module='__main__'):
+    def __init__(self, objs: _abc.Iterable=[], module: str='__main__') -> None:
         """Initializes this watch. Raises `TypeError` if `objs` is not a list of strings"""
         if not isinstance(objs, (list, set, tuple)) \
                 or any(not isinstance(obj, str) for obj in objs):
@@ -70,13 +77,13 @@ class Watch(object):
         self.objs = objs
         self.module = module
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Returns the string representation of this watch"""
         if not self:
             return '%s()' % (self.__class__.__name__,)
         return '%s(%r)' % (self.__class__.__name__, self.objs)
 
-    def add(self, var):
+    def add(self, var: str) -> None:
         """Adds the given object to this watch"""
         if not isinstance(var, str):
             raise TypeError('var must be the string name of the object')
@@ -85,36 +92,39 @@ class Watch(object):
 
         self.objs.append(var)
 
-    def get_dict(self):
+    def get_dict(self) -> dict:
         """Returns the dict for this watch"""
         return _sys.modules[self.module].__dict__
 
-    def is_watching(self, name):
+    def is_watching(self, name: str) -> bool:
         """Returns True if this watch is watching the given name, False otherwise"""
         return name in self.objs
 
-    def remove(self, var):
-        """Removes the given object from this Watch"""
+    def remove(self, var: object) -> None:
+        """Removes the given object(s) from this Watch"""
         if isinstance(var, (list, set, tuple)):
             for v in var:
                 self.objs.remove(v)
         elif isinstance(var, str):
             self.objs.remove(var)
 
-    def start_recording(self, useLocals):
+    def start_recording(self, useLocals: bool) -> None:
         """
         Records new objects created after this point until `end_recording` is called
 
         """
         self.__start_locals = useLocals.copy().keys()
 
-    def stop_recording(self, useLocals):
+    def stop_recording(self, useLocals: bool) -> None:
         """Adds the new objects since the recording started to this Watch"""
         unique = (x for x in useLocals if x not in self.__start_locals)
         for var in unique:
             self.add(var)
 
-    def view(self, transformer=lambda x: x, useLocals=None, sort=False):
+    def view(self,
+            transformer: _abc.Callable=lambda x: x,
+            useLocals: bool=None,
+            sort: bool=False) -> None:
         """Prints out the key-value pairs for this Watch"""
         groupdict = self.get_dict()
         if useLocals:
@@ -128,7 +138,7 @@ class Watch(object):
             value = transformer(groupdict[ob])
             print('{}: {} = {}'.format(ob, type(value).__name__, value))
 
-    def write_file(self, filename):
+    def write_file(self, filename: str) -> None:
         """Writes this Watch to the given file"""
         string = 'stat_dict = {}'.format(self.get_dict())
         with open(filename,'w') as fp:
@@ -151,7 +161,7 @@ if __name__ == '__main__':
     from tkinter import Button, _test
 
     class A:
-        def B(self):
+        def method(self) -> None:
             pass
 
     qualify_tests = {
@@ -164,9 +174,9 @@ if __name__ == '__main__':
         ],
         'local': [
             (A, 'A'),
-            (A.B, 'A.B'),
+            (A.method, 'A.method'),
             (A(), 'A'),
-            (A().B, 'A.B'),
+            (A().method, 'A.method'),
             (qualify, 'qualify')
         ]
     }
