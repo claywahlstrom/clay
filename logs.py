be615432cb48be2ca9477284dc7f984f2697d860
Clayton Wahlstrom <clay.wahlstrom@gmail.com>
2021-08-10 20:28:38 -0700

    Add type annotations to logs module to improve usability

--- logs.py
+++ logs.py
@@ -12,20 +12,20 @@ class Logger:
 
     """Used log messages to a log file"""
 
-    def __init__(self, name, filename, log_fmt=LOG_FMT):
+    def __init__(self, name: str, filename: str, log_fmt: str=LOG_FMT) -> None:
         """Initializes this Logger with the given name, filename, and format"""
         self.name = name
         self.filename = filename
         self.log_fmt = log_fmt
         self._last_end = '\n'
 
-    def clear(self):
+    def clear(self) -> None:
         """Clears the log file"""
         print('Clearing log {}...'.format(self.name))
         with open(self.filename, 'w'):
             pass
 
-    def log(self, message, end='\n'):
+    def log(self, message: str, end: str='\n') -> None:
         """Adds the given log message to the log file"""
         if self._last_end == '\n':
             now = dt.datetime.now()
